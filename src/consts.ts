29b2a367b29c02cad4e3cad627f7bda392e10c6d
CodeByOmm <patelomc33@gmail.com>
2026-01-10 12:23:59 +0530

    fix(favicon): update favicon paths in site.webmanifest and consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -45,7 +45,6 @@ export const SITE_METADATA = {
   icons: {
     icon: [
       { url: '/favicon/favicon.ico', sizes: '48x48' },
-      { url: '/favicon/favicon.svg', type: 'image/svg+xml' },
       { url: '/favicon/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
       { url: '/favicon/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
       { url: '/favicon/android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
